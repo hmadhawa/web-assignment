066508a7fc872ff62169d0cd8a414833bf35b91b
aasandali <aassansali@students.nsbm.ac.lk>
2021-06-12 00:09:39 +0530

    ECO page 4th commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,18 +29,20 @@
 
           <div class="row">
             <div class="col-md-4"> 
+            <div class="bg-success">
             <div class="card border-dark mb-3" style="max-width: 40rem;">
               <div class="card mb-4 box-shadow">
               <p alt="UNICEF"></p>
                 <div class="card-body">
                     <img src="https://e7.pngegg.com/pngimages/989/708/png-clipart-unicef-logo-unicef-graphics-logo-invitational-banquet.png" width="325" height="375">
-                  <p class="card-text">The impact of digital technologies (responsible for 4% of greenhouse gases) is approximately split into the production and consumption stages. At the consumption phase, 80% of its impact comes from video, which has a huge carbon footprint. But how polluting can video really be? What types of video is being consumed the most? </p>
+                  <p class="font-italic">The impact of digital technologies (responsible for 4% of greenhouse gases) is approximately split into the production and consumption stages. At the consumption phase, 80% of its impact comes from video, which has a huge carbon footprint. But how polluting can video really be? What types of video is being consumed the most? </p>
                   <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                       <a href="https://www.unicef.org/srilanka"class="btn btn-success"> View</a>
                       <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
-                  </div>
+                    </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -51,7 +53,7 @@
               <p alt="SOLOCAL"></p>
                 <div class="card-body">
                 <img src="https://static.wixstatic.com/media/9c3e51_73503a71674c4d0bb01693852b7860fc~mv2.png/v1/fill/w_289,h_199,al_c,q_90/file.jpg" width="325" height="375">
-                  <p class="card-text">Since 2013, Solocal has been engaged in a corporate social responsibility strategy. In 2018, compliance with the European Directive on the Publication of Extra-Financial Information has led Solocal to re-examine its main CSR risks and opportunities in order to publish its Statement on Non-Financial Performance (SNFP).</p>
+                  <p class="font-italic">Since 2013, Solocal has been engaged in a corporate social responsibility strategy. In 2018, compliance with the European Directive on the Publication of Extra-Financial Information has led Solocal to re-examine its main CSR risks and opportunities in order to publish its Statement on Non-Financial Performance (SNFP).</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                     <a href="https://www.solocal.com/"class="btn btn-success"> View</a>
@@ -68,7 +70,7 @@
               <p alt="The British Council"></p> 
                 <div class="card-body">
                 <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/british-council-1-logo.png" width="325" height="425">
-                  <p class="card-text">The British Council builds connections, understanding and trust between people in the UK and other countries through arts and culture, education and the English language. ... Our work in arts and culture stimulates creative expression and exchange and nurtures creative enterprise.</p>
+                  <p class="font-italic">The British Council builds connections, understanding and trust between people in the UK and other countries through arts and culture, education and the English language. ... Our work in arts and culture stimulates creative expression and exchange and nurtures creative enterprise.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <a href="https://www.britishcouncil.lk/" class="btn btn-success">View</a>
@@ -85,7 +87,7 @@
                 <p alt="Asia Foundation"></p>
                 <div class="card-body">
                 <img src="https://pbs.twimg.com/profile_images/803961894486888448/noMrOXzj.jpg" width="325" height="325">
-                  <p class="card-text">The Asia Foundation is a nonprofit international development organization committed to improving lives across a dynamic and developing Asia. Informed by six decades of experience and deep local expertise, our work across the region addresses five overarching goals—strengthen governance, empower women, expand economic opportunity, increase environmental resilience, and promote international cooperation.</p>
+                  <p class="font-italic">The Asia Foundation is a nonprofit international development organization committed to improving lives across a dynamic and developing Asia. Informed by six decades of experience and deep local expertise, our work across the region addresses five overarching goals—strengthen governance, empower women, expand economic opportunity, increase environmental resilience, and promote international cooperation.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <a href="https://asiafoundation.org/where-we-work/sri-lanka/"class="btn btn-success">View</a>
@@ -102,7 +104,7 @@
                 <p alt="US Agency for International Development(USAID)"></p>
                 <div class="card-body">
                 <img src="https://www.pikpng.com/pngl/m/495-4958147_usaid-logo-png-transparent-png-logo-de-usaid.png" width="325" height="445">
-                  <p class="card-text">USAID transforms. It transforms families, communities, and countries – so they can thrive and prosper. Whether by preventing the next global epidemic, responding to a devastating earthquake, or helping a farmer access tools to grow her business.</p>
+                  <p class="font-italic">USAID transforms. It transforms families, communities, and countries – so they can thrive and prosper. Whether by preventing the next global epidemic, responding to a devastating earthquake, or helping a farmer access tools to grow her business.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <a href="https://www.usaid.gov/sri-lanka" class="btn btn-success">View</a>
@@ -119,7 +121,7 @@
                 <p alt="Peace Brigades International."></p>
                 <div class="card-body">
                 <img src="https://betterplace-assets.betterplace.org/uploads/organisation/profile_picture/000/001/495/fill_200x200_bp1581590941_pbi_claim.jpg" width="325" height="375">
-                  <p class="card-text">Human rights defenders – courageous people who stand up for the rights and dignity of their communities because they believe in peace and justice for all – are at the heart of all we do. We use our international presence and global networks to protect, support and enable the work of human rights defenders and local activists for peaceful social change.
+                  <p class="font-italic">Human rights defenders – courageous people who stand up for the rights and dignity of their communities because they believe in peace and justice for all – are at the heart of all we do. We use our international presence and global networks to protect, support and enable the work of human rights defenders and local activists for peaceful social change.
 </p> 
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
@@ -131,23 +133,11 @@
               </div>
             </div>
             </div>
-            
           </div>
         </div>
       </div>
-
     </main>
-
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-      </div>
-    </footer>
-
-    
-
+    <p class="text-right"><a href="#">Back to top</a></p>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
