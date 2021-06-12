839aa49c7d8230419edbcf486804a707c12b6209
aasandali <aassansali@students.nsbm.ac.lk>
2021-06-13 00:10:05 +0530

    ECO page 5th commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,19 +9,20 @@
 
 
     <title>ECO Page</title>
-
    
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+
+    
     <!-- Custom styles for this template -->
 
     <link href="album.css" rel="stylesheet">
   </head>
 
   <body>
-
+  <div style="background-image:https://blog.drinktec.com/wp-content/uploads/2020/01/corporate-social-responsibility-umwelt-klimaschutz-1024x484.jpeg" width: 50px; height: 50px;></div>
     <main role="main">
 
       <div class="album py-5 bg-light">
@@ -29,18 +30,17 @@
 
           <div class="row">
             <div class="col-md-4"> 
-            <div class="bg-success">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-              <p alt="UNICEF"></p>
+            <div class="card border-dark mb-3 bg-success">
+              <div class="card mb-4" >
+         
+              <h5 class="card-header">UNICEF</h5>
                 <div class="card-body">
-                    <img src="https://e7.pngegg.com/pngimages/989/708/png-clipart-unicef-logo-unicef-graphics-logo-invitational-banquet.png" width="325" height="375">
-                  <p class="font-italic">The impact of digital technologies (responsible for 4% of greenhouse gases) is approximately split into the production and consumption stages. At the consumption phase, 80% of its impact comes from video, which has a huge carbon footprint. But how polluting can video really be? What types of video is being consumed the most? </p>
+                    <img src="https://e7.pngegg.com/pngimages/989/708/png-clipart-unicef-logo-unicef-graphics-logo-invitational-banquet.png" width="325" height="375" class="card-img-top" alt="https://e7.pngegg.com/pngimages/989/708/png-clipart-unicef-logo-unicef-graphics-logo-invitational-banquet.png">
+                  <p class="font-italic">The impact of digital technologies (responsible for 4% of greenhouse gases) is approximately split into the production and consumption stages. At the consumption phase, 80% of its impact comes from video, which has a huge carbon footprint.The agency is among the most widespread and recognizable social welfare organizations in the world.</p>
                   <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
-                      <a href="https://www.unicef.org/srilanka"class="btn btn-success"> View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
+                      <a href="https://www.unicef.org/srilanka"class="btn btn-success btn-lg"> View</a>
+                      <a href="#" class="btn btn-success btn-lg">Edit</a>
                     </div>
                     </div>
                   </div>
@@ -48,16 +48,16 @@
               </div>
             </div>
             <div class="col-md-4">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-              <p alt="SOLOCAL"></p>
+            <div class="card border-dark mb-3 bg-success">
+              <div class="card mb-4 ">
+              <h5 class="card-header">SOLOCAL</h5>
                 <div class="card-body">
-                <img src="https://static.wixstatic.com/media/9c3e51_73503a71674c4d0bb01693852b7860fc~mv2.png/v1/fill/w_289,h_199,al_c,q_90/file.jpg" width="325" height="375">
+                <img src="https://static.wixstatic.com/media/9c3e51_73503a71674c4d0bb01693852b7860fc~mv2.png/v1/fill/w_289,h_199,al_c,q_90/file.jpg" width="325" height="375" class="card-img-top" alt="https://static.wixstatic.com/media/9c3e51_73503a71674c4d0bb01693852b7860fc~mv2.png/v1/fill/w_289,h_199,al_c,q_90/file.jpg">
                   <p class="font-italic">Since 2013, Solocal has been engaged in a corporate social responsibility strategy. In 2018, compliance with the European Directive on the Publication of Extra-Financial Information has led Solocal to re-examine its main CSR risks and opportunities in order to publish its Statement on Non-Financial Performance (SNFP).</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                    <a href="https://www.solocal.com/"class="btn btn-success"> View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    <a href="https://www.solocal.com/"class="btn btn-success btn-lg">View</a>
+                    <a href="#" class="btn btn-success btn-lg">Edit</a>
                     </div>
                   </div>
                 </div>
@@ -65,16 +65,16 @@
             </div>
             </div>
             <div class="col-md-4">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-              <p alt="The British Council"></p> 
+            <div class="card border-dark mb-3 bg-success">
+              <div class="card mb-4 ">
+              <h5 class="card-header">The British Council</h5>
                 <div class="card-body">
-                <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/british-council-1-logo.png" width="325" height="425">
+                <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/british-council-1-logo.png" width="325" height="425" class="card-img-top" alt="https://cdn.freebiesupply.com/logos/thumbs/2x/british-council-1-logo.png">
                   <p class="font-italic">The British Council builds connections, understanding and trust between people in the UK and other countries through arts and culture, education and the English language. ... Our work in arts and culture stimulates creative expression and exchange and nurtures creative enterprise.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <a href="https://www.britishcouncil.lk/" class="btn btn-success">View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <a href="https://www.britishcouncil.lk/" class="btn btn-success btn-lg">View</a>
+                      <a href="#" class="btn btn-success btn-lg">Edit</a>
                     </div>
                   </div>
                 </div>
@@ -82,16 +82,16 @@
             </div>
             </div>
             <div class="col-md-4">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-                <p alt="Asia Foundation"></p>
+            <div class="card border-dark mb-3 bg-success">
+              <div class="card mb-4 ">
+              <h5 class="card-header">Asia Foundation</h5>
                 <div class="card-body">
-                <img src="https://pbs.twimg.com/profile_images/803961894486888448/noMrOXzj.jpg" width="325" height="325">
+                <img src="https://pbs.twimg.com/profile_images/803961894486888448/noMrOXzj.jpg" width="325" height="325" class="card-img-top" alt="https://pbs.twimg.com/profile_images/803961894486888448/noMrOXzj.jpg">
                   <p class="font-italic">The Asia Foundation is a nonprofit international development organization committed to improving lives across a dynamic and developing Asia. Informed by six decades of experience and deep local expertise, our work across the region addresses five overarching goals—strengthen governance, empower women, expand economic opportunity, increase environmental resilience, and promote international cooperation.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <a href="https://asiafoundation.org/where-we-work/sri-lanka/"class="btn btn-success">View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <a href="https://asiafoundation.org/where-we-work/sri-lanka/"class="btn btn-success btn-lg">View</a>
+                      <a href="#" class="btn btn-success btn-lg">Edit</a>
                     </div>
                     </div>
                   </div>
@@ -99,16 +99,16 @@
               </div>
             </div>
             <div class="col-md-4">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-                <p alt="US Agency for International Development(USAID)"></p>
+            <div class="card border-dark mb-3 bg-success">
+              <div class="card mb-4 ">
+              <h5 class="card-header">USAID</h5>
                 <div class="card-body">
-                <img src="https://www.pikpng.com/pngl/m/495-4958147_usaid-logo-png-transparent-png-logo-de-usaid.png" width="325" height="445">
+                <img src="https://www.pikpng.com/pngl/m/495-4958147_usaid-logo-png-transparent-png-logo-de-usaid.png" width="325" height="445" class="card-img-top" alt="https://www.pikpng.com/pngl/m/495-4958147_usaid-logo-png-transparent-png-logo-de-usaid.png">
                   <p class="font-italic">USAID transforms. It transforms families, communities, and countries – so they can thrive and prosper. Whether by preventing the next global epidemic, responding to a devastating earthquake, or helping a farmer access tools to grow her business.</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <a href="https://www.usaid.gov/sri-lanka" class="btn btn-success">View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <a href="https://www.usaid.gov/sri-lanka" class="btn btn-success btn-lg">View</a>
+                      <a href="#" class="btn btn-success btn-lg">Edit</a>
                     </div>
                   </div>
                   </div>
@@ -116,17 +116,18 @@
               </div>
             </div>
             <div class="col-md-4">
-            <div class="card border-dark mb-3" style="max-width: 40rem;">
-              <div class="card mb-4 box-shadow">
-                <p alt="Peace Brigades International."></p>
+            <div class="card border-dark mb-3  bg-success" >
+              <div class="card mb-4 ">
+              <h5 class="card-header">Peace Brigades International</h5>
                 <div class="card-body">
-                <img src="https://betterplace-assets.betterplace.org/uploads/organisation/profile_picture/000/001/495/fill_200x200_bp1581590941_pbi_claim.jpg" width="325" height="375">
-                  <p class="font-italic">Human rights defenders – courageous people who stand up for the rights and dignity of their communities because they believe in peace and justice for all – are at the heart of all we do. We use our international presence and global networks to protect, support and enable the work of human rights defenders and local activists for peaceful social change.
+                <img src="https://betterplace-assets.betterplace.org/uploads/organisation/profile_picture/000/001/495/fill_200x200_bp1581590941_pbi_claim.jpg" width="325" height="375" class="card-img-top" alt="https://betterplace-assets.betterplace.org/uploads/organisation/profile_picture/000/001/495/fill_200x200_bp1581590941_pbi_claim.jpg">
+                  <p class="font-italic">Human rights defenders- courageous people who stand up for the rights and dignity of their communities because they believe in peace and justice for all are at the heart of all we do. We use our international presence and global networks to protect,support and enable the work of human rights defenders are at the heart of all we do at PBI.local activists for peaceful social change.
 </p> 
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <a href="https://www.peacebrigades.org/"class="btn btn-success">View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    <a href="https://www.peacebrigades.org/" class="btn btn-success btn-lg" >View</a>
+                    <a href="#" class="btn btn-success btn-lg">Edit</a>
+                
                     </div>
                   </div>
                 </div>
