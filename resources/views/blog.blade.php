30039b717f8f2d1ed5c5d4321eb90cb8f8babed1
dmspdassanayake <dmspdassanayake@students.nsbm.ac.lk>
2021-06-15 21:22:45 +0530

    blog page fifth commit

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -37,10 +37,10 @@
         <div class="col-md-6">
             <div class="about_taital">
                 <h4 class="date_card1">April 20, 2021 | 9:00AM</h4>
-                <h1 class="about_text_card13">CARD TITLE</h1>
+                <h1 class="about_text_card134">CARD TITLE</h1>
 
-                <h5 class="by13">By: Climate Change</h5>
-                <p class="long_text_card13">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                <h5 class="by134">By: Climate Change</h5>
+                <p class="long_text_card134">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                    </p>
                 <div class="about_bt_card1"><a href="#">Read More &rarr;</a></div>
             </div>
@@ -49,7 +49,7 @@
   </div>
 
 
-  <div class="about_section_card2 about_main_card23">
+  <div class="about_section_card2 about_main_card234">
     <div class="row">
         <div class="col-md-6">
             <div class="about_taital">
@@ -69,7 +69,7 @@
 </div>
     <!-- about section end -->
 
-    <div class="about_section_card3 about_main_card23">
+    <div class="about_section_card3 about_main_card234">
   <div class="row">
     <div class="col-md-6">
       <div class="about_img"><img src="images/about-bg.png"></div>
@@ -78,10 +78,10 @@
       <div class="col-md-6">
           <div class="about_taital">
             <h4 class="date_card3">April 20, 2021 | 9:00AM</h4>
-              <h1 class="about_text_card13">CARD TITLE</h1>
+              <h1 class="about_text_card134">CARD TITLE</h1>
 
-              <h5 class="by13">By: Climate Change</h5>
-              <p class="long_text_card13">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+              <h5 class="by134">By: Climate Change</h5>
+              <p class="long_text_card134">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                   </p>
               <div class="about_bt_card3"><a href="#">Read More &rarr;</a></div>
           </div>
@@ -89,7 +89,24 @@
   </div>
 </div>   
 
+<div class="about_section_card4 about_main_card234">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="about_taital">
+                <h4 class="date_card4">April 20, 2021 | 9:00AM</h4>
+                <h1 class="about_text_card134">CARD TITLE</h1>
 
+                <h5 class="by134">By: Climate Change</h5>
+                <p class="long_text_card134">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                   </p>
+                <div class="about_bt_card4"><a href="#">Read More &rarr;</a></div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="about_img"><img src="images/about-bg.png"></div>
+        </div>
+    </div>
+</div>
     
 
 
