6e87390459a99501db237c2a0fcdf6043326108c
dmspdassanayake <dmspdassanayake@students.nsbm.ac.lk>
2021-06-18 17:36:31 +0530

    blog page sixth commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
