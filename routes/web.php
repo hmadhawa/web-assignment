46cedc5b673f876108ceb6c5277cae72748c8b3b
pasnpathiraja <pasnpathiraja@students.nsbm.ac.lk>
2021-06-18 22:23:26 +0530

    home page sixth commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/homepage', function () {
-    return view('homepage');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
