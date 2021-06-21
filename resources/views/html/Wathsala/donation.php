e60e267d919d35a5b8cb60e09203d15df584246f
wathsalaggg <72685782+wathsalaggg@users.noreply.github.com>
2021-06-21 22:15:36 +0530

    donation commit-11

--- resources/views/html/Wathsala/donation.php
+++ resources/views/html/Wathsala/donation.php
@@ -1,4 +1,13 @@
 <?php
-
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+$scity = $_POST['scity'];
+$city = $_POST['city'];
+$zip = $_POST['zip'];
+$text = $_POST['text'];
+$amount = $_POST['amount'];
+$area = $_POST['area'];
 
 ?>
