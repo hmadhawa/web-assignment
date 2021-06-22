e9869bbb17fde9aafa98936506eddbd55bdbb043
wathsalaggg <72685782+wathsalaggg@users.noreply.github.com>
2021-06-22 22:17:03 +0530

    donation commit-12

--- resources/views/html/Wathsala/donation.php
+++ resources/views/html/Wathsala/donation.php
@@ -5,9 +5,24 @@ $address = $_POST['address'];
 $phone = $_POST['phone'];
 $scity = $_POST['scity'];
 $city = $_POST['city'];
-$zip = $_POST['zip'];
+$date = $_POST['date'];
 $text = $_POST['text'];
 $amount = $_POST['amount'];
 $area = $_POST['area'];
+
+//database connection
+$conn = new mysqli('localhost', 'root', '', 'test');
+if($conn -> connect_error){
+    die('Connection Failed : ' . $conn-> connect_error);
+}
+else{
+    $stmt = $conn-> prepare("insert into donation(fname, lname, address, phone, scity, city, date, text, amount,area)
+    values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt-> bind_param("sssissssds", $fname, $lname, $address, $phone, $scity, $city, $date, $text, $amount, $area);
+    $stmt-> execute();
+    echo "Form Submitted Successfully...!";
+    $stmt-> close();
+    $conn-> close();
+}
 
 ?>
