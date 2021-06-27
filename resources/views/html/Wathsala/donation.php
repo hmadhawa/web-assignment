f7529e988bccca178fbf72c0b52cdfc60f58960e
wathsalaggg <72685782+wathsalaggg@users.noreply.github.com>
2021-06-27 22:47:30 +0530

    donation commit-17

--- resources/views/html/Wathsala/donation.php
+++ resources/views/html/Wathsala/donation.php
@@ -2,16 +2,16 @@
 
 //first code
 //------------------------------------------------------------------------------------------------
-$fname = $_POST['first_name'];
-$lname = $_POST['last_name'];
-$gender = $_POST['gender'];
-$address = $_POST['email'];
-$phone = $_POST['phone'];
-$scity = $_POST['address'];
-$country = $_POST['country'];
-$date = $_POST['date'];
-$amount = $_POST['amount'];
-$area = $_POST['comments']; 
+$fname = $_POST['first_name']??"";
+$lname = $_POST['last_name'] ??"";
+$gender = $_POST['gender']??"" ;
+$address = $_POST['email']??"";
+$phone = $_POST['phone']??"";
+$scity = $_POST['address']??"";
+$country = $_POST['country']??"";
+$date = $_POST['date']??"";
+$amount = $_POST['amount']??"";
+$area = $_POST['comments']??""; 
 
 /*
 
@@ -37,13 +37,13 @@ $con = mysqli_connect("localhost", "root", "");
 mysqli_select_db($con, "WEB-ASSIGNMENT");
 if($con){
     //echo "connection successful";
-    $stmt = $conn-> prepare("insert into donation(first_name, last_name , gender, email, phone, address, country, date, amount, comments)
+    $stmt = $con-> prepare("insert into donation(first_name, last_name , gender, email, phone, address, country, date, amount, comments)
     values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
     $stmt-> bind_param("ssisssssds", $fname, $lname, $gender, $address, $phone, $scity, $country, $date, $amount, $area);
     $stmt-> execute();
     echo "Form Submitted Successfully...!";
     $stmt-> close();
-    $conn-> close();
+   // $con-> close();
 }
 else{
     echo "connection fail". mysqli_connect_error();
